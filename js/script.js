b377b0771411f2fa9ec291b301cea8e8ca5a69c9
varFox <31412438+varFox@users.noreply.github.com>
2019-04-30 17:38:32 +0300

    2

--- js/script.js
+++ js/script.js
@@ -34,3 +34,19 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   });
 });
+
+
+let goTo = document.querySelectorAll('.container ul li a');
+
+goTo.forEach(item => {
+  item.addEventListener('click', function (e) {
+    e.preventDefault(); 
+    let blockID = item.getAttribute('href');
+    console.log(item.getAttribute('href'));
+    
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });  
+})
