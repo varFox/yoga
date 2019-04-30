428f9efbf225aee40d802c62810b3d22a940cdfe
varFox <31412438+varFox@users.noreply.github.com>
2019-04-30 20:45:53 +0300

    3

--- js/script.js
+++ js/script.js
@@ -33,53 +33,71 @@ window.addEventListener('DOMContentLoaded', function() {
       }
     }
   });
+  // прокрутка (scroll)
+  let goTo = document.querySelectorAll('.container ul li a');
+
+  goTo.forEach(item => {
+    item.addEventListener('click', function (e) {
+      e.preventDefault(); 
+      let blockID = item.getAttribute('href');
+      console.log(item.getAttribute('href'));
+      
+      document.querySelector('' + blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });  
+  })
+
+  // таймер
+  function zero(a) {
+    if (a < 10) {
+      a = '0' + a;
+    }
+    return a;
+  }
+
+  let deadline = '2019-10-21';
+
+  function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+        hs = Math.floor(t /(1000 * 60 * 60)),
+        ms = Math.floor((t / 1000 / 60) % 60),
+        ss = Math.floor((t / 1000) % 60);
+    return {
+      'total' : t,
+      'hours' : hs,
+      'minutes' : ms,
+      'seconds' : ss
+    };
+  }
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        timeIntervar = setInterval(updateClock, 1000);
+    function updateClock() {
+      let t = getTimeRemaining(endtime);
+      hours.textContent = zero(t.hours) ;
+      console.log(t.hours);
+      minutes.textContent = zero(t.minutes);
+      seconds.textContent = zero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeIntervar);
+        hours.textContent = '00';
+        minutes.textContent = '00';
+        seconds.textContent = '00';
+      }
+    }
+  }
+
+  setClock('timer', deadline);
 });
 
-// прокрутка (scroll)
-let goTo = document.querySelectorAll('.container ul li a');
-
-goTo.forEach(item => {
-  item.addEventListener('click', function (e) {
-    e.preventDefault(); 
-    let blockID = item.getAttribute('href');
-    console.log(item.getAttribute('href'));
-    
-    document.querySelector('' + blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    });
-  });  
-})
 
 
-// таймер
-function zero(a) {
-  if (a < 10) {
-    a = '0' + a;
-  }
-  return a;
-}
 
-const timer = document.querySelector('#timer'),
-      hs = timer.querySelector('.hours'),
-      ms = timer.querySelector('.minutes'),
-      ss = timer.querySelector('.seconds');
 
-function time(h, m, s) {
-  let hours = h,
-      minutes = m,
-      seconds = s;     
-  let t = setInterval(function() {
-    hs.textContent = zero(hours);
-    ms.textContent = zero(minutes);
-    ss.textContent = zero(seconds);
-    (seconds > 0) ? seconds -=1 : (seconds == 0 && minutes > 0) 
-                  ? (seconds = 59, minutes -= 1) : (seconds == 0 && minutes == 0 && hours > 0) 
-                  ? (seconds = 59, minutes = 59, hours -= 1) 
-                  : (clearInterval(t), hs.textContent = '00', ms.textContent = '00', ss.textContent = '00');
-    
-
-  }, 1000);
-}
-
-time(10, 0, 0);
